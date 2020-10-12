0007cbccb911080fcc7bbd4660600fc99e1d5c76
lachezarg <lgeorgieff@gmail.com>
2020-10-12 21:00:06 +0300

    03 - Nested Conditional Statements complete

--- lib.py
+++ lib.py
@@ -1,3 +1,25 @@
+'''
 def percentage(part, whole):
     return 100 * float(part) / float(whole)
 
+
+def inrange (xr,yr, xr1, yr1,  xr2, yr2 ):
+    if (xr == xr1 or x == xr2) and (yr >= yr1 and y <= yr2):
+        return True
+    else:
+        return False
+
+def inborder (xr,yr, xr1, yr1,  xr2, yr2 ):
+    if inrange(xr,yr, xr1, yr1,  xr2, yr2) or inrange(yr, xr, yr1, xr1, yr2, xr2):
+        return True
+    else
+        return False
+
+# Instide in rectangle - not used
+def inside(xr, yr, xr1, yr1, xr2, yr2):
+    if (xr >= xr1 and x <= xr2) and (yr > yr1 and y < yr2):
+        return True
+    else:
+        return False
+
+'''
