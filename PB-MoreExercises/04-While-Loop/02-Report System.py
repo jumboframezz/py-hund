c447af441abeea381b3cbdc1dcd5afa83bfb29eb
lucho <lgeorgieff@gmail.com>
2020-10-14 08:35:51 +0300

    More mess

--- PB-MoreExercises/04-While-Loop/02-Report System.py
+++ PB-MoreExercises/04-While-Loop/02-Report System.py
@@ -20,40 +20,44 @@
 #               Плащанията трябва да бъдат форматирани до втората цифра след десетичния знак.
 #     • При получаване на команда "End", да се изпише един ред:
 #     o "Failed to collect required money for charity."
-#https://judge.softuni.bg/Contests/Practice/Index/1684#1
+# https://judge.softuni.bg/Contests/Practice/Index/1684#1
 
 target_sum = int(input())
-
-sale_counter = 1
-cache_sum = 0
-cashe_count = 0
-card_sum = 0
-card_count = 0
-total_sales = 0
-num_sales = 0
-while total_sales <= target_sum:
-    if sale_counter % 2 != 0:
-        if sale_num > 100:
+total = 0
+deal_counter = 0
+cache_deal_counter = 0
+card_deal_counter = 0
+cache_deal_amount = 0
+card_deal_amount = 0
+sale = input()
+while total <= target_sum:
+    price = int(sale)
+    deal_counter += 1
+    if deal_counter % 2 != 0:
+        if price > 100:
             print("Error in transaction!")
             continue
         else:
-            cache_sum += sale_num
-            total_sales += sale_num
-            cashe_count += 1
+            total += price
+            cache_deal_counter += 1
+            cache_deal_amount += price
             print("Product sold!")
     else:
-        if sale_num < 10
+        if price < 10:
             print("Error in transaction!")
             continue
-        else
-            car_sum += sale_num
-            total_sales += sale_num
-            card_count += 1
+        else:
+            total += price
+            card_deal_counter += 1
+            card_deal_amount += 1
             print("Product sold!")
-    sale_num = int(input())
-    if sale_num == "End":
+
+    if total >= target_sum:
+        break
+    sale = input()
+    if sale == "End":
         print("Failed to collect required money for charity.")
         exit(0)
 
-"Average CS: {средно плащане в кеш на човек}"
-"Average CC: {средно плащане с карта на човек}"
+print(f"Average CS: {cache_deal_amount / cache_deal_counter:.2f}")
+print(f"Average CC: {card_deal_amount / card_deal_counter:.2f}")
