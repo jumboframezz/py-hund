af7983c375f1264e170a245f4fc737f627c3545a
lachezarg <lgeorgieff@gmail.com>
2020-10-21 00:34:48 +0300

    06 - Drawing figures initial

---
+++ PB-MoreExercises/06-DrawingFiguresWithLoops/01-RectangleOf10x10Stars.py
@@ -0,0 +1,9 @@
+# 1.	Правоъгълник от 10 x 10 звездички
+# Напишете програма, която чертае на конзолата правоъгълник от 10 x 10 звездички.
+# judje link:  https://judge.softuni.bg/Contests/Practice/Index/1851#0
+
+
+for i in range(0, 10):
+    for ii in range(0, 10):
+        print("*", end="")
+    print("")
---
+++ PB-MoreExercises/06-DrawingFiguresWithLoops/test.py
@@ -0,0 +1,12 @@
+
+
+n = 5
+
+parity = None
+
+parity = True if n % 2 == 0 else False
+print(parity)
+
+x = 3
+x += (2+2) if False else 10
+print(x)
