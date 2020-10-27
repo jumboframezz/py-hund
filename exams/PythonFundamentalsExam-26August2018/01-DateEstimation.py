b344f83882f3078bfd693b050771fca81883a107
lachezarg <lgeorgieff@gmail.com>
2020-10-27 15:51:40 +0200

    Fund exam practice 01

--- exams/PythonFundamentalsExam-26August2018/01-DateEstimation.py
+++ exams/PythonFundamentalsExam-26August2018/01-DateEstimation.py
@@ -17,3 +17,30 @@
 # Input           Output
 # 2018-08-20      Passed
 # 2021-08-26      1097 days left
+# URL: https://judge.softuni.bg/Contests/1140/Python-Fundamentals-Exam-26-August-2018
+import time
+import datetime
+
+def days_between(d1, d2):
+    from datetime import datetime
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return (d2 - d1).days
+
+
+exam_date = "2018-08-26"
+in_date = input()
+diff = days_between(exam_date, in_date)
+
+if diff < 0:
+    print("Passed")
+elif diff == 0:
+    print("Today date")
+elif diff > 0:
+    print(f"{diff + 1} days left")
+
+
+
+
+
+
---
+++ exams/PythonFundamentalsExam-26August2018/02-Lists.py
@@ -0,0 +1,16 @@
+# Problem 2. Lists
+# Input / Constraints
+# You will be given a single lines of elements(integers), separated with one or more spaces. You should check if all
+# elements in the line are unique. If they are you should increase the value of every even element with the number of
+# 2 and print the list on single row in ascending order separated by ",".
+# If they are not unique you should increase every odd element with the number of 3 and print them on single row,
+# separated with ":"
+# On the next line you should print sum of the all elements divided by the count of the elements in the list. You should
+# do that until you receive the command "stop playing"
+# Output
+# If the elements are unique
+# Unique list: {elements in the list, separated by “,”}
+# Output: {sum of all elements divided by the length of the list}
+# Else
+# Non-unique list: {elements in the list, separated by “:”}
+# Output: {sum of all elements divided by the length of the list}
