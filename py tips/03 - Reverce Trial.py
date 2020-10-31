c56ecca9cc8df5c59953d1f920d7a95ed36497c5
lachezarg <lgeorgieff@gmail.com>
2020-10-31 20:10:30 +0200

    10 - list basics completed

--- py tips/03 - Reverce Trial.py
+++ py tips/03 - Reverce Trial.py
@@ -1,10 +1,18 @@
 
 
-arr = [f"element {_}" for _ in range(10)]
+# arr = [f"element {_}" for _ in range(10)]
+#
+#
+# print(f"arr: {arr}")
+# print(f"len: {len(arr)}")
+#
+# for i in range(len(arr) - 1, -1, -1 ):
+#     print(arr[i])
+#
 
 
-print(f"arr: {arr}")
-print(f"len: {len(arr)}")
 
-for i in range(len(arr) - 1, -1, -1 ):
-    print(arr[i])
+arr = [1, 2, 3, 4, 5 ,6]
+for _ in range(len(arr)):
+    print(f"index: {_} : {arr[_]} {len(arr)}")
+    arr.remove(arr[_])
